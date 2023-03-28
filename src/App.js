21278cfe466b7bde8f8de1f6d1e87fcc9d48f899
kaushik roy <kaushikroy1001@gmail.com>
2023-03-28 23:01:46 +0530

    added fetching api

--- src/App.js
+++ src/App.js
@@ -3,17 +3,33 @@ import Navbar from "./components/Navbar/Navbar";
 import Carousel from "./components/Carousel/Carousel";
 import Cateogory from "./components/Cateogory/Cateogory"
 import NewsCard from "./components/NewsCard/NewsCard"
+import search from "./asset/search.png"
 import "./index.css";
-
-
+import { useEffect } from "react";
+// require ('dotenv').config();
 function App() {
+  useEffect(() => {
+    var url = 'https://newsapi.org/v2/everything?q=Apple&from=2023-03-28&sortBy=popularity&apiKey=' + process.env.REACT_APP_API_KEY;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+
+  }, [])
+  
+
+
   return (
   <div className="app">
     <Navbar />
     <Carousel />
     <Cateogory />
     <NewsCard />
-    <div className="exploreButton">Explore</div>
+    <div className="exploreButton">
+    <img src={search} alt="search" />
+    Explore</div>
+   
   </div>
   );
 }
